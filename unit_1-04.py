af2146c77d2a5d8d22b516e8c4aa7b2e34328a60
Ubuntu <ubuntu@ip-172-31-22-149.ec2.internal>
2019-10-04 18:42:03 +0000

    added new fail

---
+++ unit_1-04.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Created by: Manuel Garcia Yuste
+# Created on : October 2019
+# This program calculates the area and perimeter of a rectangle
+#     with dimension 5cm x 3cm
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
